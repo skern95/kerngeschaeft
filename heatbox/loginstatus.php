e6aa25d15c21a36b196eab0edecfeddf9a1b65ae
chofmann98 <constantin.hofmann@gmx.net>
2017-05-24 11:38:01 +0200

    Update loginstatus.php

--- heatbox/loginstatus.php
+++ heatbox/loginstatus.php
@@ -2,7 +2,7 @@
 						session_start();
 						if (!empty($_SESSION['user'])){
 						echo '
-						<li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">',$_SESSION['user']['email'],' <i class="icon-angle-down"></i></a>
+						<li class="dropdown login"><a href="#" class="dropdown-toggle" data-toggle="dropdown">',$_SESSION['user']['email'],' <i class="icon-angle-down"></i></a>
 							<ul class="dropdown-menu">
 								<li><a href="logout.php">Logout</a></li>
 							</ul>
