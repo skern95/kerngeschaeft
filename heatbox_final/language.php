d96ddb59c71fa4967772914e318aff094349b533
skern95 <basti-kern@gmx.de>
2017-06-06 11:25:56 +0200

    Update language.php

--- heatbox_final/language.php
+++ heatbox_final/language.php
@@ -2,11 +2,11 @@
 	function toggleLanguage() {
 		if($_SESSION['sprachnr'] == 1) {
 			echo '
-			<button class="btn btn-transparent" style="border: none"  name="englisch" value="2"><i class="icon"><img src="images/ico/eng.png" style="width: 20px"/></i></button>
+			<button class="btn btn-transparent" style="border: none"  name="englisch" value="2"><i class="icon"><img src="images/ico/ger.png" style="width: 20px"/></i></button>
 			';
 		} else if($_SESSION['sprachnr'] == 2) {
 			echo '
-			<button class="btn btn-transparent" style="border: none"  name="deutsch" value="1"><i class="icon"><img src="images/ico/ger.png" style="width: 20px"/></i></button>
+			<button class="btn btn-transparent" style="border: none"  name="deutsch" value="1"><i class="icon"><img src="images/ico/eng.png" style="width: 20px"/></i></button>
 			';
 		}
 	}
