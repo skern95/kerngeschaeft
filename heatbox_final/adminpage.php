aa725d9e283270da092287063e5c5486d3f34e1a
skern95 <basti-kern@gmx.de>
2017-06-06 08:22:46 +0200

    Update adminpage.php

--- heatbox_final/adminpage.php
+++ heatbox_final/adminpage.php
@@ -317,43 +317,5 @@ echo '
 	<script src="js/vendor/jquery-1.9.1.min.js"></script>
 	<script src="js/vendor/bootstrap.min.js"></script>
 	<script src="js/main.js"></script>
-	<!-- Required javascript files for Slider -->
-	<script src="js/jquery.ba-cond.min.js"></script>
-	<script src="js/jquery.slitslider.js"></script>
-	<!-- /Required javascript files for Slider -->
-
-	<!-- SL Slider -->
-	<script type="text/javascript"> 
-		$(function() {
-			var Page = (function() {
-
-				var $navArrows = $( '#nav-arrows' ),
-				slitslider = $( '#slider' ).slitslider( {
-					autoplay : true
-				} ),
-
-				init = function() {
-					initEvents();
-				},
-				initEvents = function() {
-					$navArrows.children( ':last' ).on( 'click', function() {
-						slitslider.next();
-						return false;
-					});
-
-					$navArrows.children( ':first' ).on( 'click', function() {
-						slitslider.previous();
-						return false;
-					});
-				};
-
-				return { init : init };
-
-			})();
-
-			Page.init();
-		});
-	</script>
-	<!-- /SL Slider -->
 </body>
 </html>
